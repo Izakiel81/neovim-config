68f3f5f7fd26fc370e6c8e40a96ee1dd7996a659
Maksym Habinskyi <mgabin348@gmail.com>
2025-08-30 17:37:09 +0200

    Minor tweaks

--- lua/plugins/mason-lsp-config.lua
+++ lua/plugins/mason-lsp-config.lua
@@ -1,6 +1,6 @@
 return {
   "mason-org/mason-lspconfig.nvim",
-  opts = { "lua_ls", "vimls", "ts_ls", "pyright", "gopls", "html", "cssls", "jsonls", "bshls" },
+  opts = { "lua_ls", "vimls", "ts_ls", "pyright", "gopls", "html", "cssls", "jsonls", "bahsls" },
   dependencies = {
     { "mason-org/mason.nvim", opts = {} },
     "neovim/nvim-lspconfig",
