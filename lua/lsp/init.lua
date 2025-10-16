2b74848c70c9d577b8702e76a21d272130ce64cc
Maksym Habinskyi <mgabin348@gmail.com>
2025-10-16 11:43:13 +0200

    Python support fix

--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -2,8 +2,6 @@ local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.textDocument.completion.completionItem.snippetSupport = true
 
 local servers = {
-  "pyright",
-  "lua_ls",
   "ts_ls",
   "vue_ls",
   "html",
